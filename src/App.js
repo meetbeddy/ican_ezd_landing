6446b3faf9d8c1405d3a96137461e5049f774dbc
Obed Okpala <katlybedrick@gmail.com>
2023-01-01 18:17:32 +0100

    add 404 page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Landing from "./landing/Index";
 import Register from "./auth/Register";
 import RegSuccess from "./auth/RegSuccess";
 import AboutPage from "./landing/pages/AboutPage";
+import PageNotFound from "./landing/pages/404Page";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Landing />}></Route>
         <Route path="/about" element={<AboutPage />} />
         <Route path="/register" element={<Register />}></Route>
